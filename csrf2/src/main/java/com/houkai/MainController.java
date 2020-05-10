c9811bb14a046d7cbfadc8b60ad50714d5fbfa9d
houkai <hikoukay@163.com>
2020-05-10 09:29:30 +0800

    csrf及xss网络攻击理解与学习

--- csrf2/src/main/java/com/houkai/MainController.java
+++ csrf2/src/main/java/com/houkai/MainController.java
@@ -6,11 +6,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainController {
     /**
-     * http://localhost:8081/
+     * http://localhost:8081/list
      * @return
      */
-    @GetMapping
+    @GetMapping("list")
     public String list(){
         return "list";
+    }
+    /**
+     * http://localhost:8081/update
+     * @return
+     */
+    @GetMapping("update")
+    public String update(){
+        return "update";
     }
 }
